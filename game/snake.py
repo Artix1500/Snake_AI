3bdd1152762960dd55db5d922f733d1067e2ca46
Patryk <patryk.gsrmeen@gmail.com>
2019-04-01 09:59:27 +0200

    Modify game to run it with agent

--- game/snake.py
+++ game/snake.py
@@ -2,13 +2,13 @@ import pygame
 import random
 import sys
 
-FPS = 15
+ENABLE_KEYBOARD = True
 BLOCK_SIZE = 50
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 800
 BOARD_HEIGHT = SCREEN_HEIGHT // BLOCK_SIZE
 BOARD_WIDTH = SCREEN_WIDTH // BLOCK_SIZE
-KEY = {"UP": 1, "DOWN": 2, "LEFT": 3, "RIGHT": 4}
+KEY = {"UP": 1, "DOWN": 2, "LEFT": 3, "RIGHT": 4, "YES": 5, "NO": 6, "EXIT": 7}
 
 
 def check_collision(pos1, pos2):
@@ -98,8 +98,8 @@ class Snake:
         self.direction = direction
 
     def check_crash(self):
-        if self.elements[0].x < 0 or self.elements[0].y < 0 or self.elements[0].x >= BOARD_WIDTH or self.elements[
-            0].y >= BOARD_HEIGHT:
+        if self.elements[0].x < 0 or self.elements[0].y < 0 or self.elements[0].x >= BOARD_WIDTH or \
+                self.elements[0].y >= BOARD_HEIGHT:
             return True
 
         counter = 1
@@ -122,34 +122,41 @@ class Snake:
                 return True
         return False
 
-    def display_log(self):
-        print("Segments: ", end="")
-        for segment in self.elements:
-            print("({0}, {1}) ".format(segment.x, segment.y), end="")
 
+class Game:
+    def __init__(self):
+        self.score = 0
+        self.main_snake = Snake(1, 1)
+        self.apple_eaten = False
+        self.apple = self.spawn_apple()
+        self.running = True
+        self.last_info = self.get_info()
+        self.grow_snake = False
+
+    def get_info(self):
         closest_right = 1
         closest_left = 1
         closest_up = 1
         closest_down = 1
-        head = self.elements[0]
+        head = self.main_snake.elements[0]
 
         while closest_right <= BOARD_WIDTH - head.x:
-            if self.collides_with_body(Segment(head.x + closest_right, head.y)):
+            if self.main_snake.collides_with_body(Segment(head.x + closest_right, head.y)):
                 break
             closest_right += 1
 
         while closest_left <= head.x + 1:
-            if self.collides_with_body(Segment(head.x - closest_left, head.y)):
+            if self.main_snake.collides_with_body(Segment(head.x - closest_left, head.y)):
                 break
             closest_left += 1
 
         while closest_down <= BOARD_HEIGHT - head.y:
-            if self.collides_with_body(Segment(head.x, head.y + closest_down)):
+            if self.main_snake.collides_with_body(Segment(head.x, head.y + closest_down)):
                 break
             closest_down += 1
 
         while closest_up <= head.y + 1:
-            if self.collides_with_body(Segment(head.x, head.y - closest_up)):
+            if self.main_snake.collides_with_body(Segment(head.x, head.y - closest_up)):
                 break
             closest_up += 1
 
@@ -158,156 +165,143 @@ class Snake:
         closest_down = min(closest_down, BOARD_HEIGHT - head.y)
         closest_up = min(closest_up, head.y + 1)
 
-        crashes = {
-            "right": closest_right,
-            "left": closest_left,
-            "down": closest_down,
-            "up": closest_up
+        apple_x_dist = abs(self.main_snake.elements[0].x - self.apple.x)
+        apple_y_dist = abs(self.main_snake.elements[0].y - self.apple.y)
+
+        info = {
+            "right_crash": closest_right,
+            "left_crash": closest_left,
+            "down_crash": closest_down,
+            "up_crash": closest_up,
+            "reward": 0,
+            "lost_game": False,
+            "apple_x": apple_x_dist,
+            "apple_y": apple_y_dist
         }
 
-        print("\nRight crash: {0}, left crash: {1}, up crash: {2}, down crash: {3}.".format(
-            crashes["right"], crashes["left"], crashes["up"], crashes["down"]))
+        print("\nRight crash: {0}, left crash: {1}, up crash: {2}, "
+              "down crash: {3}. Reward: {4}, lost game: {5}. Apple x: {6}, "
+              "apple y: {7}.".format(info["right_crash"], info["left_crash"], info["up_crash"],
+                                     info["down_crash"], info["reward"], info["lost_game"],
+                                     info["apple_x"], info["apple_y"]))
 
+        return info
 
-def read_key():
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                return KEY["UP"]
-            elif event.key == pygame.K_DOWN:
-                return KEY["DOWN"]
-            elif event.key == pygame.K_LEFT:
-                return KEY["LEFT"]
-            elif event.key == pygame.K_RIGHT:
-                return KEY["RIGHT"]
-            elif event.key == pygame.K_ESCAPE:
-                return "exit"
-            elif event.key == pygame.K_y:
-                return "yes"
-            elif event.key == pygame.K_n:
-                return "no"
-        if event.type == pygame.QUIT:
-            sys.exit()
+    def wait_for_action(self):
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    return KEY["UP"]
+                elif event.key == pygame.K_DOWN:
+                    return KEY["DOWN"]
+                elif event.key == pygame.K_LEFT:
+                    return KEY["LEFT"]
+                elif event.key == pygame.K_RIGHT:
+                    return KEY["RIGHT"]
+                elif event.key == pygame.K_ESCAPE:
+                    return KEY["EXIT"]
+                elif event.key == pygame.K_y:
+                    return KEY["YES"]
+                elif event.key == pygame.K_n:
+                    return KEY["NO"]
+            if event.type == pygame.QUIT:
+                sys.exit()
 
+    def spawn_apple(self):
+        valid_apple = False
+        x = 0
+        y = 0
+        while not valid_apple:
+            valid_apple = True
+            x = random.randint(0, BOARD_WIDTH - 1)
+            y = random.randint(0, BOARD_HEIGHT - 1)
+            for segment in self.main_snake.elements:
+                if check_collision(segment, Segment(x, y)):
+                    valid_apple = False
+                    break
 
-def wait_for_key():
-    while True:
-        event = pygame.event.wait()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                return KEY["UP"]
-            elif event.key == pygame.K_DOWN:
-                return KEY["DOWN"]
-            elif event.key == pygame.K_LEFT:
-                return KEY["LEFT"]
-            elif event.key == pygame.K_RIGHT:
-                return KEY["RIGHT"]
-            elif event.key == pygame.K_ESCAPE:
-                return "exit"
-            elif event.key == pygame.K_y:
-                return "yes"
-            elif event.key == pygame.K_n:
-                return "no"
-        if event.type == pygame.QUIT:
-            sys.exit()
+        return Apple(x, y, True)
 
+    def end_game(self):
+        message = game_over_font.render("Game Over", 1, pygame.Color("white"))
+        message_play_again = play_again_font.render("Restart? Y / N", 1, pygame.Color("green"))
+        main_screen.blit(message, (320, 240))
+        main_screen.blit(message_play_again, (320 + 12, 240 + 40))
 
-def spawn_apple(snake):
-    valid_apple = False
-    x = 0
-    y = 0
-    while not valid_apple:
-        valid_apple = True
-        x = random.randint(0, BOARD_WIDTH - 1)
-        y = random.randint(0, BOARD_HEIGHT - 1)
-        for segment in snake.elements:
-            if check_collision(segment, Segment(x, y)):
-                valid_apple = False
+        pygame.display.flip()
+        pygame.display.update()
+
+        my_key = self.wait_for_action()
+        print("MY KEY GOT IS: {0}".format(my_key))
+        while my_key != KEY["EXIT"]:
+            if my_key == KEY["YES"]:
+                print("RUNNING GAME!!!!!!")
+                self.run_game()
+            elif my_key == KEY["NO"]:
                 break
+            my_key = self.wait_for_action()
+        sys.exit()
 
-    return Apple(x, y, True)
+    def draw_score(self):
+        score_area = score_area_font.render(str(self.score), 1, pygame.Color("white"))
+        main_screen.blit(score_msg, (SCREEN_WIDTH - score_msg_size[0] - 60, 10))
+        main_screen.blit(score_area, (SCREEN_WIDTH - 45, 10))
 
+    def redraw_game(self):
+        main_screen.fill(background_color)
+        if self.apple.exists:
+            self.apple.draw(main_screen)
+        self.main_snake.draw(main_screen)
+        self.draw_score()
+        pygame.display.flip()
+        pygame.display.update()
 
-def end_game():
-    message = game_over_font.render("Game Over", 1, pygame.Color("white"))
-    message_play_again = play_again_font.render("Restart? Y / N", 1, pygame.Color("green"))
-    main_screen.blit(message, (320, 240))
-    main_screen.blit(message_play_again, (320 + 12, 240 + 40))
-
-    pygame.display.flip()
-    pygame.display.update()
-
-    my_key = read_key()
-    while my_key != "exit":
-        if my_key == "yes":
-            run_game()
-        elif my_key == "no":
-            break
-        my_key = read_key()
-    sys.exit()
-
-
-def draw_score(score):
-    score_area = score_area_font.render(str(score), 1, pygame.Color("white"))
-    main_screen.blit(score_msg, (SCREEN_WIDTH - score_msg_size[0] - 60, 10))
-    main_screen.blit(score_area, (SCREEN_WIDTH - 45, 10))
-
-
-def redraw_game(apple, snake, score):
-    main_screen.fill(background_color)
-    if apple.exists:
-        apple.draw(main_screen)
-    snake.draw(main_screen)
-    draw_score(score)
-    pygame.display.flip()
-    pygame.display.update()
-
-
-def run_game():
-    score = 0
-    main_snake = Snake(1, 1)
-    apple_eaten = False
-    apple = spawn_apple(main_snake)
-    running = True
-
-    while running:
-
-        # Draw game
-        redraw_game(apple, main_snake, score)
-
-        # Check collisions (walls and self)
-        if main_snake.check_crash():
-            end_game()
-
-        # Check apple availability
-        grow_snake = False
-        if apple.exists:
-            if check_collision(main_snake.get_head(), apple):
-                grow_snake = True
-                apple.exists = False
-                score += 5
-                apple_eaten = True
-
-        # Spawn apple
-        if apple_eaten:
-            apple_eaten = False
-            apple = spawn_apple(main_snake)
-            print("Wow, you've eaten an apple! Next apple: ({0}, {1})".format(apple.x, apple.y))
-            redraw_game(apple, main_snake, score)
-
-        # Wait for user input (here goes agent's move)
-        main_snake.display_log()
-        print("Waiting for input...")
-        key_pressed = wait_for_key()
-        if key_pressed == "exit":
-            running = False
+    def action(self, action_key):
+        if action_key == KEY["EXIT"]:
+            self.running = False
 
         # Move snake
-        if grow_snake:
-            main_snake.grow()
-        if key_pressed:
-            main_snake.set_direction(key_pressed)
-        main_snake.move()
+        if self.grow_snake:
+            self.main_snake.grow()
+        if action_key:
+            self.main_snake.set_direction(action_key)
+        self.main_snake.move()
+        return self.last_info
+
+    def run_game(self):
+        self.__init__()
+        while self.running:
+
+            # Draw game
+            self.redraw_game()
+
+            # Check collisions (walls and self)
+            if self.main_snake.check_crash():
+                print("OH NO, COLLISION")
+                self.end_game()
+
+            # Check apple availability
+            self.grow_snake = False
+            if self.apple.exists:
+                if check_collision(self.main_snake.get_head(), self.apple):
+                    self.grow_snake = True
+                    self.apple.exists = False
+                    self.score += 5
+                    self.apple_eaten = True
+
+            # Spawn apple
+            if self.apple_eaten:
+                self.apple_eaten = False
+                self.apple = self.spawn_apple()
+                self.redraw_game()
+
+            # Wait for user input (here goes agent's move)
+            print("Waiting for action...")
+            if ENABLE_KEYBOARD:
+                action_key = self.wait_for_action()
+            self.action(action_key)
+            self.last_info = self.get_info()
 
 
 if __name__ == "__main__":
@@ -329,4 +323,6 @@ if __name__ == "__main__":
     apple_image = pygame.transform.scale(pygame.image.load("apple.png").convert_alpha(), (BLOCK_SIZE, BLOCK_SIZE))
     snake_image = pygame.transform.scale(pygame.image.load("snake_box.jpg").convert_alpha(), (BLOCK_SIZE, BLOCK_SIZE))
 
-    run_game()
+    # Game initialization
+    game = Game()
+    game.run_game()
