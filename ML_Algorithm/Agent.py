ebf1bda6a003a3d26dbc0a2e839802b50484f0cc
Krzysztof Kamiński <cryptoethic@gmail.com>
2019-05-14 15:31:21 +0200

    give reward after final move

--- ML_Algorithm/Agent.py
+++ ML_Algorithm/Agent.py
@@ -14,13 +14,16 @@ class Agent:
         self.state_list = deque(maxlen=10000)
         self.predict_counter = 0
         
-    def get_action(self, state, previous_reward):
+    def get_action(self, state, previous_reward, game_over=False):
         # reshape says that it is just one sample
         # -1 means that it will fit the size to make it right
         self.predict_counter += 1
         output_state = np.asarray(state).reshape(1, -1)
         if self.previous_state is not None:
             self.state_list.append((self.previous_state, previous_reward, self.previous_action))
+        if game_over:
+            print ("Game over!")
+            return
         self.previous_state = output_state
         self.previous_action = self.model.predict(output_state)
         return self.previous_action
