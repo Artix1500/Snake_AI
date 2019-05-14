85d95a0d4bf13e73fece46486f7a27671047e942
Artix1500 <artur.haczek23@gmail.com>
2019-05-14 14:51:32 +0200

    Add Epsilon moves

--- ML_Algorithm/Agent.py
+++ ML_Algorithm/Agent.py
@@ -1,17 +1,27 @@
 import numpy as np
+import random
 
 from Model import Model
 
 class Agent:
 
-    def __init__(self):
+    def __init__(self,
+                 epsilon=1,
+                 epsilon_rate=0.99):
+
+        self.epsilon = epsilon
+        self.epsilon_rate = epsilon_rate
+        self.min_epsilon = 0.2
         self.model = Model()
 
     def get_action(self, state):
         output_state = np.asarray(state)
-        # reshape says that it is just one sample
-        # -1 means that it will fit the size to make it right
-        return self.model.predict(output_state.reshape(1, -1))
+        q_values = self.model.predict(output_state.reshape(1, -1))
+
+        if (random.random() < self.epsilon):
+            return random.randint(1, 4)
+        else:
+            return q_values
 
     def train(self):
         self.model.train()
