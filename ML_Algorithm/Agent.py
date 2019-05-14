d3bf4389361ba9df8c672d225ec8c6cb84dd8fa8
krzypkowskap student.mini.pw.edu.pl <krzypkowskap@student.mini.pw.edu.pl>
2019-05-14 15:52:02 +0200

    Add batch sizes for training model

--- ML_Algorithm/Agent.py
+++ ML_Algorithm/Agent.py
@@ -13,7 +13,10 @@ class Agent:
         self.previous_action = None
         self.state_list = deque(maxlen=10000)
         self.predict_counter = 0
-        
+
+        self.batch_size = 1024
+        self.mini_batch_size = 32
+
     def get_action(self, state, previous_reward, game_over=False):
         # reshape says that it is just one sample
         # -1 means that it will fit the size to make it right
@@ -30,7 +33,11 @@ class Agent:
         self.previous_action = self.model.predict(output_state)
         return self.previous_action
 
+    def get_batch():
+        return random.sample(list(self.state_list), self.batch_size)
+
     def train(self):
-        print("Training our model!!!!")
-        self.model.train()
+        batch = self.get_batch()
+        for i in range(self.batch_size/self.mini_batch_size):
+            self.model.train(batch[i*self.mini_batch_size:(i+1)*self.mini_batch_size])
         self.model.decreaseEpsilon()
